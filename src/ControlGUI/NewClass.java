354f1114f7c472b1cdc1711e86507d0a7c79a8da
MiguelAnchico <mangel.anchico@gmail.com>
2022-02-24 10:52:04 -0500

    NuevaClase
    Agregue una nueva clase con atributo tipo String

---
+++ src/ControlGUI/NewClass.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ControlGUI;
+
+/**
+ *
+ * @author miguel.anchico
+ */
+public class NewClass {
+    private String Agregandoalgonuevo;
+
+    public String getAgregandoalgonuevo() {
+        return Agregandoalgonuevo;
+    }
+
+    public void setAgregandoalgonuevo(String Agregandoalgonuevo) {
+        this.Agregandoalgonuevo = Agregandoalgonuevo;
+    }
+    
+    
+}
