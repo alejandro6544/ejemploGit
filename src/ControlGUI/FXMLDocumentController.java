6b8b03a5461ad9753df358929262c1d7dcbdb8f8
alejandro6544 <alejandro6544@gmail.com>
2022-02-24 11:54:27 -0500

    Lienzo
    Creación de canvas y figuras geometricas

--- src/ControlGUI/FXMLDocumentController.java
+++ src/ControlGUI/FXMLDocumentController.java
@@ -10,7 +10,10 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
 
 /**
  *
@@ -18,18 +21,46 @@ import javafx.scene.control.Label;
  */
 public class FXMLDocumentController implements Initializable {
     
-    @FXML
-    private Label label;
+//    @FXML
+//    private Label label;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private Canvas lienzo;
+    
+    GraphicsContext g;
+    
+    @FXML
+    private void bCirculo(ActionEvent event) {
+        
+       g.setStroke(Color.BLUE);
+        g.setLineWidth(3);
+        g.strokeOval(0, 0, 100, 100);
+        g.setFill(Color.CHOCOLATE);
+        g.fillOval(0, 0, 100, 100);
+        
+        
     }
-    
+    @FXML
+    private void bTriangulo(ActionEvent event) {
+        double x[]={50,100,0};
+        double y[]={0,100,100};
+        g.setStroke(Color.BLUE);
+        g.setLineWidth(3);
+        g.strokePolygon(x, y, 3);
+    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        g=lienzo.getGraphicsContext2D();
+        
+        double h=lienzo.getHeight();
+        double w=lienzo.getWidth();
+        
+        g.setStroke(Color.AQUAMARINE);
+        g.setLineWidth(3);
+        g.strokeRect(0, 0, w, h);
+        
     }    
+    
+    
     
 }
